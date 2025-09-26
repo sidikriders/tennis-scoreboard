7188a642a8bcc7607169b4b2472c09bf65c8a22a
sidikss <sidik@sourcesage.co>
2025-09-26 23:28:30 +0700

    initiate use-query-params

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { StringParam, useQueryParams, withDefault } from 'use-query-params';
+import Home from './Home';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queryParams] = useQueryParams({
+    page: withDefault(StringParam, 'home')
+  });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-        <h1 className="text-4xl font-bold text-blue-500">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  console.log(queryParams.page)
+
+  if (queryParams.page === 'home') {
+    return <Home />;
+  }
+
+  return null;
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { QueryParamProvider } from 'use-query-params';
+import { BrowserRouter } from 'react-router-dom';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <QueryParamProvider adapter={ReactRouter6Adapter}>
+        <App />
+      </QueryParamProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
